578a100d0d5e09ddd61c853baaaee83921358723
Gilbert Yankey <86690944+yankeygilbert@users.noreply.github.com>
2023-03-25 19:16:59 +0100

    Add files via upload

---
+++ contract.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract certToken
+{
+// structure for certiface storage 
+    struct user_token{
+        string id; 
+        bool exit;
+    }
+
+// mapping for certifcate storage 
+
+    mapping (string  => user_token)  alltoken;
+
+    // function to add certficate 
+
+    function addToken(string memory _id, string memory _token) public 
+    {
+     
+        alltoken[_token] = user_token(_id,true);
+    }
+
+    //function to check if certificate exist 
+
+     function isMappingObjectExists(string memory _token) public view returns (bool) {
+       if (alltoken[_token].exit == true)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+     }
+}
